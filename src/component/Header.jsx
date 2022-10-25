198f2432731b9373a2694269de4730b5f611194e
Fardin Khan_451 <designerfardin2021@gmail.com>
2022-10-25 09:33:15 +0600

    update

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,88 +1,77 @@
 import React from "react";
+import { IoIosMenu, IoMdClose } from "react-icons/io";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [state, setstate] = useState(false);
+
+  let navBtn = (state) => {
+    setstate(!state);
+  };
   return (
-    <div className='drawer sm:h-[64px] h-[65px]'>
-      <input id='my-drawer-3' type='checkbox' className='drawer-toggle' />
-      <div className='drawer-content flex flex-col'>
-        <div className='w-full navbar bg-base-300'>
-          <div className='flex-none lg:hidden'>
-            <label htmlFor='my-drawer-3' className='btn btn-square btn-ghost'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                className='inline-block w-6 h-6 stroke-current'>
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M4 6h16M4 12h16M4 18h16'></path>
-              </svg>
-            </label>
-          </div>
-          <div className='flex-1 px-2 mx-2'>
-            <Link to='/'>
-              <div className='flex items-center font-bold text-xl'>
-                <img
-                  src='https://img.icons8.com/fluency/48/000000/student-center.png'
-                  alt='navIcon'
-                />
-                Student Center
-              </div>
-            </Link>
-          </div>
-          <div className='flex-none hidden lg:block'>
-            <ul className='menu menu-horizontal'>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/courses'>Courses</Link>
-              </li>
-              <li>
-                <Link to='/faq'>FAQ</Link>
-              </li>
-              <li>
-                <Link to='/blog'>Blog</Link>
-              </li>
-              <li>
-                <Link to='/login'>Login</Link>
-              </li>
-              <div className='avatar placeholder'>
-                <div className='bg-neutral-focus text-neutral-content rounded-full w-12'>
-                  <span>MX</span>
-                </div>
-              </div>
-            </ul>
-          </div>
+    <div className='navbar bg-base-100'>
+      <div className='navbar-start'>
+        <div className='dropdown'>
+          <label tabIndex={0} className='btn btn-ghost btn-circle'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'>
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h16M4 18h7'
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/courses'>Courses</Link>
+            </li>
+            <li>
+              <Link to='/faq'>FAQ</Link>
+            </li>
+            <li>
+              <Link to='/blog'>Blog</Link>
+            </li>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+           
+          </ul>
         </div>
       </div>
-      <div className='drawer-side'>
-        <label htmlFor='my-drawer-3' className='drawer-overlay'></label>
-        <ul className='menu p-4 overflow-y-auto w-80 bg-base-100'>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/courses'>Courses</Link>
-          </li>
-          <li>
-            <Link to='/faq'>FAQ</Link>
-          </li>
-          <li>
-            <Link to='/blog'>Blog</Link>
-          </li>
-          <li>
-            <Link to='/login'>Login</Link>
-          </li>
-          <div className='avatar placeholder'>
-            <div className='bg-neutral-focus text-neutral-content rounded-full w-12'>
-              <span>MX</span>
+      <div className='navbar-center'>
+        <a className='btn btn-ghost normal-case text-xl'>
+          <Link to='/' className="flex items-center">
+            <img
+              src='https://img.icons8.com/fluency/48/000000/student-center.png'
+              alt='nav'
+            />
+            Student Center
+          </Link>
+        </a>
+      </div>
+      <div className='navbar-end'>
+        <button className='btn btn-ghost btn-square p-2'>
+          <Link to='/login'>Login</Link>
+        </button>
+        <button className='btn btn-ghost btn-circle'>
+        <div className='avatar placeholder'>
+              <div className='bg-neutral-focus text-neutral-content rounded-full w-12'>
+                <span>MX</span>
+              </div>
             </div>
-          </div>
-        </ul>
+        </button>
       </div>
     </div>
   );
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            this is home
+  return (
+    <div
+      className='hero min-h-screen'
+      style={{
+        backgroundImage: `url("https://i.ibb.co/HtL4QMg/pexels-christina-morillo-1181671.jpg")`,
+      }}>
+      <div className='hero-overlay bg-opacity-60'></div>
+      <div className='hero-content text-center text-neutral-content'>
+        <div className='max-w-md'>
+          <h1 className='mb-4 text-5xl font-bold'>
+            Hello there. Welcome to student center
+          </h1>
+          <p className='mb-5 font-medium text-2xl'>
+          Take a journey with us. Toward your successful career. Use our content to enrich your knowledge in your field.
+          </p>
+          <button className='btn btn-primary'>
+            <Link to='/reg'>Get Started</Link>
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
